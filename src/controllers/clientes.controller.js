29511aa6167b21259b6215227a3f3de194daa7b2
KimberlingCruz <cruzkimberling81@gmail.com>
2025-09-09 10:45:59 -0600

    se registra nueva categoria

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -12,3 +12,21 @@ export const obtenerClientes = async (req, res) => {
     });
   }
 };
+
+// Obtener un cliente por su ID
+export const obtenercliente = async (req, res) => {
+  try {
+    const id_cliente = req.params.id_cliente;
+    const [result] = await pool.query('SELECT * FROM cliente WHERE id_cliente= ?', [req.params.id_cliente])
+    if (result.length <= 0) {
+      return res.status(404).json({
+        mensaje: `Error al leer los datos. ID ${id_cliente} no encontrado.`
+      });
+    }
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al leer los datos de las cliente.'
+    });
+  }
+};
